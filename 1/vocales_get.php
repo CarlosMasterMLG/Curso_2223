7dc3854b29697fba7665588e52854e028ba4a2ea
carlos_blanco <carlosblancofranco@gmail.com>
2022-10-11 20:33:52 +0200

    validar que es una vocal un parametro

--- 1/vocales_get.php
+++ 1/vocales_get.php
@@ -9,9 +9,29 @@
 <body>
     
 <?php
+    // Mostrar errores a medida que se ejecutan: ini_set
+    ini_set('display_errors', 'On');
+    ini_set('html_errors', 0);
 
-    function esVocal($char){
-        return in_array($char, array("a", "e", "i", "o", "u"));
+    $letra = $_GET["letra"];
+
+    echo "La letra introducida es " . $letra . "<br>";
+
+    $vocales = array("a","e","i","o","u");
+    $loop = false;
+
+    for ($i=0; $i < count($vocales); $i++){
+
+        if ($letra == $vocales[$i]){
+            $loop = true;
+        }
+
+    }
+
+    if($loop){
+        echo $letra . " es una vocal.";
+    } else {
+        echo $letra . " no es una vocal.";
     }
 
 ?>
