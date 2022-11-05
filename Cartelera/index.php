5256aae5dd945777100d5a4aaccd69e8b60f65aa
Carlos Blanco <73190277+CarlosMasterMLG@users.noreply.github.com>
2022-11-05 16:21:06 +0100

    Add files via upload

--- Cartelera/index.php
+++ Cartelera/index.php
@@ -10,12 +10,95 @@
             <a href="pagina1.php">Primera categoría</a>
             <a href="pagina2.php">Segunda categoría</a>
         </div>
+        <hr>
         <div class="segunda_caja">
 
-            <div class="primera_columna"></div>
-            <div class="segunda_columna"></div>
-            <div class="tercera_columna"></div>
+            <div class="primera_columna">
+                <img src="imgs/terror/the_thing.jpg" alt="The Thing">
+            </div>
+            <div class="segunda_columna">
+                <h1>La cosa (The Thing)</h1>
+                <p>Año: 1982</p>
+                <p>Duración: 105 min.</p>
+                <p>Dirección: John Carpenter</p>
+                <p>Guion: Bill Lancaster Novela: John W. Campbell</p>
+                <p>Música: Ennio Morricone</p>
+                <p>Fotografía: Dean Cundey</p>
+                <p>Reparto: Kurt Russell, Wilford Brimley, Keith David, David Clennon, 
+                    Richard Dysart, Donald Moffat, Richard Masur, T.K. Carter, Charles 
+                    Hallahan, Peter Maloney, Joel Polis, Thomas G. Waites</p>
+                <p>Compañías: Universal Pictures, Turman-Foster Company</p>
+                <p>Género: Ciencia ficción. Terror. Fantástico | Película de culto. Extraterrestres. Remake. Zonas frías/polares</p>
+            </div>
+            <div class="tercera_columna">
+                <p>Sinopsis<br>En una estación experimental remota de la Antártida, 
+                    un equipo de científicos de investigación estadounidenses ven 
+                    cómo en su campamento base un helicóptero noruego dispara contra 
+                    un perro de trineo. Cuando acogen al perro, éste ataca brutalmente 
+                    tanto a los seres humanos como a los caninos del campamento, y 
+                    descubren que la bestia, de origen desconocido, puede asumir la 
+                    forma de sus víctimas...</p>
+            </div>
+        </div>
+        <hr>
+        <div class="segunda_caja">
 
+            <div class="primera_columna">
+                <img src="imgs/terror/willys_wonderland.jpg" alt="Willy's Wonderland">
+            </div>
+            <div class="segunda_columna">
+                <h1>Willy's Wonderland</h1>
+                <p>Año: 2021</p>
+                <p>Duración: 88 min.</p>
+                <p>Dirección: Kevin Lewis</p>
+                <p>Guion: G.O. Parsons</p>
+                <p>Música: Émoi</p>
+                <p>Fotografía: David Newbert</p>
+                <p>Reparto: Nicolas Cage, Emily Tosta, Caylee Cowan, Beth Grant, 
+                    Terayle Hill, Grant Cramer, Ric Reitz, Chris Schmidt Jr., Duke Jackson, 
+                    David Sheftell, Christian Delgrosso, Taylor Towery, Jiri Stanek, Jonathan 
+                    Mercedes, Jessica Graves, Chris Warner, BJ Guyer, Olga Cramer, 
+                    Chris Padilla, D.J. Stavropoulos</p>
+                <p>Compañías: JD Entertainment, Landafar Entertainment, Landmark Studio Group. Distribuidora: VVS Films</p>
+                <p>Género: Terror. Comedia. Fantástico. Acción | Monstruos. Robots. Comedia de terror</p>
+            </div>
+            <div class="tercera_columna">
+                <p>Sinopsis<br>Un hombre solitario se ve obligado a quedarse en un 
+                remoto pueblo de Nevada cuando su coche se avería. Allí, un mecánico se ofrece 
+                a repararle el vehículo a cambio de que él trabaje de conserje nocturno en 
+                Willy's Wonderland, un local de entretenimiento familiar abandonado hace ya 
+                tiempo, que se acaba convirtiendo en una auténtica pesadilla. Allí tendrá que 
+                luchar contra un monstruo tras otro para sobrevivir hasta el día siguiente.</p>
+            </div>            
+        </div>
+        <hr>
+        <div class="segunda_caja">
+
+            <div class="primera_columna">
+                <img src="imgs/terror/midsommar.jpg" alt="Midsommar">
+            </div>
+            <div class="segunda_columna">
+                <h1>Midsommar</h1>
+                <p>Año: 2019</p>
+                <p>Duración: 145 min.</p>
+                <p>Dirección: Ari Aster</p>
+                <p>Guion: Ari Aster</p>
+                <p>Música: Bobby Krlic</p>
+                <p>Fotografía: Pawel Pogorzelski</p>
+                <p>Reparto: Florence Pugh, Jack Reynor, Will Poulter, William Jackson Harper, 
+                    Ellora Torchia, Archie Madekwe, Vilhelm Blomgren, Julia Ragnarsson, Anna 
+                    Åström, Anki Larsson, Lars Väringer, Katarina Weidhagen van Hal, Isabelle 
+                    Grill, Björn Andrésen</p>
+                <p>Coproducción Estados Unidos-Suecia-Hungría; B-Reel Films, Parts and Labor. Distribuidora: A24</p>
+                <p>Género: Terror. Intriga. Drama | Vida rural. Sectas</p>
+            </div>
+            <div class="tercera_columna">
+                <p>Sinopsis<br>Una pareja estadounidense que no está pasando por su mejor momento acude con 
+                unos amigos al Midsommar, un festival de verano que se celebra cada 90 años en una aldea 
+                remota de Suecia. Lo que comienza como unas vacaciones de ensueño en un lugar en el que el 
+                sol no se pone nunca, poco a poco se convierte en una oscura pesadilla cuando los misteriosos 
+                aldeanos les invitan a participar en sus perturbadoras actividades festivas.</p>
+            </div>            
         </div>
         <div class="tercera_caja"></div>
     <div>
