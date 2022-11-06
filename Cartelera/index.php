6baa0810028ac5ad33fe7a79f295cc14ade96158
Carlos Blanco <73190277+CarlosMasterMLG@users.noreply.github.com>
2022-11-06 14:58:22 +0100

    Add files via upload

--- Cartelera/index.php
+++ Cartelera/index.php
@@ -6,13 +6,13 @@
 <body>
     <div class="contenedor">
         <div class="primera_caja">
+            <h1>Carlos Productions Cinema</h1>
             <a href="index.php">INICIO</a>
             <a href="pagina1.php">Primera categoría</a>
             <a href="pagina2.php">Segunda categoría</a>
         </div>
 
         <div class="segunda_caja">
-
             <div class="primera_columna">
                 <img src="imgs/terror/the_thing.jpg" alt="The Thing">
             </div>
@@ -42,7 +42,6 @@
         </div>
         
         <div class="segunda_caja">
-
             <div class="primera_columna">
                 <img src="imgs/terror/willys_wonderland.jpg" alt="Willy's Wonderland">
             </div>
@@ -71,9 +70,125 @@
                 luchar contra un monstruo tras otro para sobrevivir hasta el día siguiente.</p>
             </div>            
         </div>
+
+        <div class="segunda_caja">
+            <div class="primera_columna">
+                <img src="imgs/terror/alien_el_octavo_pasajero.jpg" alt="Alien">
+            </div>
+            <div class="segunda_columna">
+                <h1>Alien, el octavo pasajero</h1>
+                <p>Año: 1979</p>
+                <p>Duración: 116 min.</p>
+                <p>Dirección: Ridley Scott</p>
+                <p>Guion: Dan O'Bannon. Historia: Ronald Shusett. Personaje: H.R. Giger</p>
+                <p>Música: Jerry Goldsmith</p>
+                <p>Fotografía: Derek Vanlint</p>
+                <p>Reparto: Sigourney Weaver, John Hurt, Yaphet Kotto, Tom Skerritt, Veronica Cartwright, 
+                    Harry Dean Stanton, Ian Holm</p>
+                <p>Compañías: 20th Century Fox, Brandywine Productions</p>
+                <p>Género: Ciencia ficción. Terror | Extraterrestres. Aventura espacial. Película de culto</p>
+            </div>
+            <div class="tercera_columna">
+                <p>Sinopsis<br>De regreso a la Tierra, la nave de carga Nostromo interrumpe su viaje y 
+                despierta a sus siete tripulantes. El ordenador central, MADRE, ha detectado la misteriosa 
+                transmisión de una forma de vida desconocida, procedente de un planeta cercano aparentemente 
+                deshabitado. La nave se dirige entonces al extraño planeta para investigar el origen 
+                de la comunicación.</p>
+            </div>
+        </div>
+
+        <div class="segunda_caja">
+            <div class="primera_columna">
+                <img src="imgs/terror/el_amanecer_de_los_muertos.jpg" alt="Amanecer de los muertos">
+            </div>
+            <div class="segunda_columna">
+                <h1>Amanecer de los muertos</h1>
+                <p>Año: 2004</p>
+                <p>Duración: 100 min.</p>
+                <p>Dirección: Zack Snyder</p>
+                <p>Guion: James Gunn. Remake: George A. Romero</p>
+                <p>Música: Tyler Bates</p>
+                <p>Fotografía: Matthew F. Leonetti</p>
+                <p>Reparto: Sarah Polley, Ving Rhames, Jake Weber, Mekhi Phifer, Ty Burrell, Michael Kelly, 
+                    Kevin Zegers, Michael Barry, Lindy Booth, Tom Savini, Bruce Bohne, Jayne Eastwood, 
+                    Boyd Banks, Inna Korobkina, R.D. Reid, Kim Poirier, Matt Frewer, Justin Louis, Hannah 
+                    Lochner, Ermes Blarasin, Sanjay Talwar, Kim Roberts, Tim Post, Matt Sadowski, Philip DeWilde, 
+                    Colm Magner, Luigia Zucaro, Geoff Williams, Mike Realba, Phillip MacKenzie, Laura DeCarteret, 
+                    Georgia Craig, Tino Monte, Chris Gillett, Derek Keurvorst, Dan Duran, Neville Edwards, Sandy 
+                    Jobin-Bevans, Natalie Brown, Liz West, Scott H. Reiniger, Ken Foree</p>
+                <p>Compañías: Strike Entertainment, New Amsterdam Entertainment, Metropolitan Filmexport, Toho-Towa. Distribuidora: Universal Pictures</p>
+                <p>Género: Terror. Ciencia ficción | Gore. Zombis. Remake</p>
+            </div>
+            <div class="tercera_columna">
+                <p>Sinopsis<br>Remake del filme de terror de George A. Romero. Una inexplicable plaga ha 
+                diezmado la población del planeta, convirtiendo a los muertos en horribles zombies que 
+                continuamente buscan carne y sangre humana para sobrevivir. En Wisconsin, un variopinto 
+                grupo de personas que han escapado a la plaga, tratan de salvar la vida refugiándose en 
+                un centro comercial, donde deben aprender no sólo a protegerse de las hordas de zombies, 
+                sino también a convivir. </p>
+            </div>
+        </div>
+
+        <div class="segunda_caja">
+            <div class="primera_columna">
+                <img src="imgs/terror/get_out.jpg" alt="Get Out">
+            </div>
+            <div class="segunda_columna">
+                <h1>Dejame salir</h1>
+                <p>Año: 2017</p>
+                <p>Duración: 103 min.</p>
+                <p>Dirección: Jordan Peele</p>
+                <p>Guion: Jordan Peele</p>
+                <p>Música: Michael Abels</p>
+                <p>Fotografía: Toby Oliver</p>
+                <p>Reparto: Daniel Kaluuya, Allison Williams, Catherine Keener, Bradley Whitford, 
+                    Betty Gabriel, Caleb Landry Jones, Lyle Brocato, Ashley LeConte Campbell, Marcus 
+                    Henderson, Lil Rel Howery, Jeronimo Spinx, Rutherford Cravens, Lakeith Stanfield</p>
+                <p>Compañías: Blumhouse Productions, QC Entertainment</p>
+                <p>Género: Intriga. Terror. Thriller | Thriller psicológico. Comedia negra. Racismo. Familia</p>
+            </div>
+            <div class="tercera_columna">
+                <p>Sinopsis<br>Un joven afroamericano visita a la familia de su novia blanca, un 
+                matrimonio adinerado. Para Chris y su novia Rose ha llegado el momento de conocer 
+                a los futuros suegros, por lo que ella le invita a pasar un fin de semana en el campo 
+                con sus padres, Missy  y Dean . Al principio, Chris piensa que el comportamiento "demasiado" 
+                complaciente de los padres se debe a su nerviosismo por la relación interracial de 
+                su hija, pero a medida que pasan las horas, una serie de descubrimientos cada vez 
+                más inquietantes le llevan a descubrir una verdad inimaginable.</p>
+            </div>
+        </div>
+
+        <div class="segunda_caja">
+            <div class="primera_columna">
+                <img src="imgs/terror/jaws.jpg" alt="Jaws">
+            </div>
+            <div class="segunda_columna">
+                <h1>Tiburón</h1>
+                <p>Año: 1975</p>
+                <p>Duración: 124 min.</p>
+                <p>Dirección: Steven Spielberg</p>
+                <p>Guion: Peter Benchley, Carl Gottlieb. Novela: Peter Benchley</p>
+                <p>Música: John Williams</p>
+                <p>Fotografía: Bill Butler</p>
+                <p>Reparto: Roy Scheider, Robert Shaw, Richard Dreyfuss, Lorraine Gary, 
+                    Murray Hamilton, Carl Gottlieb, Jeffrey C. Kramer, Susan Backlinie, 
+                    Jonathan Filley, Chris Rebello, Jay Mello, Craig Kingsbury, Jeffrey 
+                    Voorhees, Lee Fierro, Ted Grossman, Robert Chambers, Peter Benchley</p>
+                <p>Compañías: Zanuck/Brown. Distribuidora: Universal Pictures</p>
+                <p>Género: Terror. Drama | Animales. Tiburones. Película de culto</p>
+            </div>
+            <div class="tercera_columna">
+                <p>Sinopsis<br>En la costa de un pequeño pueblo del Este de los Estados Unidos, 
+                un enorme tiburón ataca a varias personas. Por temor a los nefastos efectos que 
+                este hecho podría tener sobre el negocio turístico, el alcalde se niega a cerrar 
+                las playas y a difundir la noticia. Pero un nuevo ataque del tiburón termina con 
+                la vida de un bañista. Cuando el terror se apodera de todos, un veterano cazador 
+                de tiburones, un oceanógrafo y el jefe de la policía local se unen para intentar 
+                capturar al escualo.</p>
+            </div>
+        </div>
         
         <div class="segunda_caja">
-
             <div class="primera_columna">
                 <img src="imgs/terror/midsommar.jpg" alt="Midsommar">
             </div>
@@ -89,7 +204,7 @@
                     Ellora Torchia, Archie Madekwe, Vilhelm Blomgren, Julia Ragnarsson, Anna 
                     Åström, Anki Larsson, Lars Väringer, Katarina Weidhagen van Hal, Isabelle 
                     Grill, Björn Andrésen</p>
-                <p>Coproducción Estados Unidos-Suecia-Hungría; B-Reel Films, Parts and Labor. Distribuidora: A24</p>
+                <p>Compañías: Coproducción Estados Unidos-Suecia-Hungría; B-Reel Films, Parts and Labor. Distribuidora: A24</p>
                 <p>Género: Terror. Intriga. Drama | Vida rural. Sectas</p>
             </div>
             <div class="tercera_columna">
@@ -98,6 +213,94 @@
                 remota de Suecia. Lo que comienza como unas vacaciones de ensueño en un lugar en el que el 
                 sol no se pone nunca, poco a poco se convierte en una oscura pesadilla cuando los misteriosos 
                 aldeanos les invitan a participar en sus perturbadoras actividades festivas.</p>
+            </div>            
+        </div>
+
+        <div class="segunda_caja">
+            <div class="primera_columna">
+                <img src="imgs/terror/the_exorcist.jpg" alt="El exorcista">
+            </div>
+            <div class="segunda_columna">
+                <h1>El exorcista</h1>
+                <p>Año: 1973</p>
+                <p>Duración: 121 min.</p>
+                <p>Dirección: William Friedkin</p>
+                <p>Guion: William Peter Blatty. Novela: William Peter Blatty</p>
+                <p>Música: Jack Nitzsche</p>
+                <p>Fotografía: Owen Roizman</p>
+                <p>Reparto: Linda Blair, Max von Sydow, Ellen Burstyn, Jason Miller, Lee J. Cobb, 
+                    Kitty Winn, Jack MacGowran, Arthur Storch, Barton Heyman, Gina Petrushka, John 
+                    Mahon, William O'Malley, Peter Masterson, Rudolf Schündler, Robert Symonds, 
+                    Thomas Bermingham, Vasiliki Maliaros, Titos Vandis, Wallace Rooney, Ron Faber, 
+                    Donna Mitchell, Roy Cooper, Robert Gerringer, Mercedes McCambridge</p>
+                <p>Compañías: Warner Bros., Hoya Productions</p>
+                <p>Género: Terror | Posesiones / Exorcismos. Religión. Sobrenatural. Película de culto</p>
+            </div>
+            <div class="tercera_columna">
+                <p>Sinopsis<br>Regan, una niña de doce años, sufre fenómenos paranormales como la 
+                levitación o la manifestación de una fuerza sobrehumana. Su madre, aterrorizada, 
+                tras someter a su hija a múltiples análisis médicos que no ofrecen ningún resultado, 
+                acude a un sacerdote con estudios de psiquiatría. Éste, convencido de que el mal no 
+                es físico sino espiritual, cree que se trata de una posesión diabólica, y decide 
+                practicar un exorcismo... Adaptación de la novela de William Peter Blatty que se 
+                inspiró en un exorcismo real ocurrido en Washington en 1949.</p>
+            </div>            
+        </div>
+
+        <div class="segunda_caja">
+            <div class="primera_columna">
+                <img src="imgs/terror/the_shining.jpg" alt="El resplandor">
+            </div>
+            <div class="segunda_columna">
+                <h1>El resplandor</h1>
+                <p>Año: 1980</p>
+                <p>Duración: 146 min.</p>
+                <p>Dirección: Stanley Kubrick</p>
+                <p>Guion: Stanley Kubrick, Diane Johnson. Novela: Stephen King</p>
+                <p>Música: Rachel Elkind, Wendy Carlos</p>
+                <p>Fotografía: John Alcott</p>
+                <p>Reparto: Jack Nicholson, Shelley Duvall, Danny Lloyd, Scatman Crothers, 
+                    Barry Nelson, Philip Stone, Joe Turkel, Lia Beldam, Billie Gibson, Barry 
+                    Dennen, David Baxt, Manning Redwood, Lisa Burns, Alison Coleridge, Norman 
+                    Gay, Tony Burton, Anne Jackson, Jana Shelden, Burnell Tucker</p>
+                <p>Compañías: Coproducción Reino Unido-Estados Unidos; Hawk Films, Peregrine, 
+                    Warner Bros., Producers Circle. Distribuidora: Warner Bros.</p>
+                <p>Género: Terror | Sobrenatural. Casas encantadas. Fantasmas. Drama psicológico. Película de culto</p>
+            </div>
+            <div class="tercera_columna">
+                <p>Sinopsis<br>Jack Torrance se traslada con su mujer y su hijo de siete años 
+                al impresionante hotel Overlook, en Colorado, para encargarse del mantenimiento 
+                de las instalaciones durante la temporada invernal, época en la que permanece 
+                cerrado y aislado por la nieve. Su objetivo es encontrar paz y sosiego para 
+                escribir una novela. Sin embargo, poco después de su llegada al hotel, al mismo 
+                tiempo que Jack empieza a padecer inquietantes trastornos de personalidad, se 
+                suceden extraños y espeluznantes fenómenos paranormales.</p>
+            </div>            
+        </div>
+
+        <div class="segunda_caja">
+            <div class="primera_columna">
+                <img src="imgs/terror/train_to_busan.jpg" alt="Train to Busan">
+            </div>
+            <div class="segunda_columna">
+                <h1>Tren a Busan</h1>
+                <p>Año: 2016</p>
+                <p>Duración: 118 min.</p>
+                <p>Dirección: Yeon Sang-ho</p>
+                <p>Guion: Yeon Sang-ho, Park Joo-suk</p>
+                <p>Música: Young-gyu Jang</p>
+                <p>Fotografía: Lee Hyung-deok</p>
+                <p>Reparto: Gong Yoo, Ma Dong-seok, Ahn So-hee, Kim Soo-an, Jeong Yu-mi, Kim 
+                    Eui-sung, Choi Woo-sik, Shim Eun-kyung, Choi Gwi-hwa, Jang Hyuk-jin, Ye 
+                    Soo-jung, Park Myeong-Shin, Jeong Seok-yong, Chang-hwan Kim, Kim Jae-rok, 
+                    Seong-soo Han, Hye-Yeong Moon, Do-im Woo</p>
+                <p>Compañías: Next Entertainment World, RedPeter Film</p>
+                <p>Género: Terror. Acción. Ciencia ficción. Drama | Trenes / Metros. Zombis. Familia</p>
+            </div>
+            <div class="tercera_columna">
+                <p>Sinopsis<br>Un virus letal se expande por Corea del Sur, provocando violentos 
+                altercados. Los pasajeros de un tren KTX que viaja de Seúl a Busan tendrán que 
+                luchar por su supervivencia.</p>
             </div>            
         </div>
         <div class="tercera_caja"></div>
