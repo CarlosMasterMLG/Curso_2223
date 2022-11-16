c4a7dc1192f69fc6a6a5fa1576d7aaf2f1f4ec2b
carlos_blanco <carlosblancofranco@gmail.com>
2022-11-16 15:50:08 +0100

    16 noviembre clase

--- Cartelera/pruebaPeliculas.php
+++ Cartelera/pruebaPeliculas.php
@@ -3,30 +3,30 @@
     <?php
         $style;
         $titulo;
+        $icon;
         $numCategoria = $_GET['categoria'];
         if ($numCategoria==1) {
-            $style = 'stylePrueba';
+            $style = 'styleTERROR';
             $titulo = 'TERROR';
+            $icon = 'imgs/ghostface_icon.png';
         } else{
             $style = 'styleCF';
             $titulo = 'CIENCIA FICCION';
+            $icon = 'imgs/darthvader_icon.png';
         }
     ?>
     <title><?php echo $titulo?></title>
     <link rel="stylesheet" href="<?php echo $style?>.css">
-    <link rel="shortut icon" type="image/png" href="imgs/ghostface_icon.png" sizes="32x32">
+    <link rel="shortut icon" type="image/png" href="<?php echo $icon?>" sizes="32x32">
     
 </head>
 <body>
-    <div class="contenedor">
-        <div class="primera_caja">
-            <h1>TERROR</h1>
-            <a href="index.php">INICIO</a>
-        </div>
-
         <?php
+
+        if ($numCategoria==1) {
+                     
         $posters = array("imgs/terror/alien_el_octavo_pasajero.jpg", "imgs/terror/el_amanecer_de_los_muertos.jpg", "imgs/terror/get_out.jpg", "imgs/terror/jaws.jpg", "imgs/terror/midsommar.jpg",
-            "imgs/terror/the_exorcist.jpg", "imgs/terror/the_shining.jpg", "imgs/terror/the_thing.jpg", "imgs/terror/train_to_busan.jpg", "imgs/terror/willys_wonderland.jpg");
+        "imgs/terror/the_exorcist.jpg", "imgs/terror/the_shining.jpg", "imgs/terror/the_thing.jpg", "imgs/terror/train_to_busan.jpg", "imgs/terror/willys_wonderland.jpg");
 
         $titulos = array("Alien", "El amanecer de los muertos", "Dejame salir", "Tiburón", "Midsommar", "El exorcista", "El resplandor", "La cosa", "Tren a Busan", "Willy's Wonderland");
 
@@ -45,16 +45,21 @@
             "Un hombre solitario (Cage) se ve obligado a quedarse en un remoto pueblo de Nevada cuando su coche se avería. Allí, un mecánico se ofrece a repararle el vehículo a cambio de que él trabaje de conserje nocturno en Willy's Wonderland, un local de entretenimiento familiar abandonado hace ya tiempo, que se acaba convirtiendo en una auténtica pesadilla. Allí tendrá que luchar contra un monstruo tras otro para sobrevivir hasta el día siguiente.");
         $puntuaciones = array(8, 6, 7, 7, 7, 8, 10, 10, 8, 7);
 
+        echo "<div class='contenedor'>
+                <div class='primera_caja'>
+                    <h1>TERROR</h1>
+                    <a href='index.php'>INICIO</a>
+                </div>";
         for ($i=0; $i < count($posters); $i++) { 
-                    
+                
             $sinopsisReducida = substr($sinopsis[$i], 0, 280);
 
             echo "<div class='segunda_caja'>
             <div class='bordeIzquierdo'></div>
             <div class='primera_columna'>
-                    <h1 class='titulo'>$titulos[$i]</h1>
-                    <img src='$posters[$i]'>
-                    <p class='duracion'>$duraciones[$i].</p>
+                <h1 class='titulo'>$titulos[$i]</h1>
+                <img src='$posters[$i]'>
+                <p class='duracion'>$duraciones[$i].</p>
             </div>
             <div class='segunda_columna'>
                 <p>
@@ -84,9 +89,87 @@
             </div>
         </div>
         <div class='bordeDerecho'></div>
-    </div>";
+        </div>";
         }
         echo "<div class='tercera_caja'></div>";
+
+        } else {
+
+            $posters = array("imgs/ciencia_ficción/akira.jpg","imgs/ciencia_ficción/back_to_the_future.jpg","imgs/ciencia_ficción/back_to_the_future_2.jpg",
+                            "imgs/ciencia_ficción/back_to_the_future_3.jpg","imgs/ciencia_ficción/desafio_total.jpg","imgs/ciencia_ficción/kung_fury.jpg",
+                            "imgs/ciencia_ficción/terminator.jpg","imgs/ciencia_ficción/terminator_2.jpg","imgs/ciencia_ficción/the_matrix.jpg",
+                            "imgs/ciencia_ficción/tron.jpg");
+
+            $titulos = array("Akira","Regreso al futuro","Regreso al futuro 2","Regreso al futuro 3","Desafio total","Kung Fury","Terminator",
+                            "Terminator 2","Matrix","Tron");
+
+            $duraciones = array("124 min.","116 min.","105 min.","118 min.","109 min.",
+                                "31 min.","108 min.","135 min.","131 min.","82 min.");
+
+            $sinopsis = array("Año 2019. Neo-Tokyo es una ciudad construida sobre las ruinas de la antigua capital japonesa destruida tras la Tercera Guerra Mundial. Japón es un país al borde del colapso que sufre continuas crisis políticas. En secreto, un equipo de científicos ha reanudado por orden del ejército un experimento para encontrar a individuos que puedan controlar el arma definitiva: una fuerza denominada 'la energía absoluta'. Pero los habitantes de Neo-Tokyo tienen otras cosas de las que preocuparse. Uno de ellos es Kaneda, un joven pandillero líder de una banda de motoristas. Durante una pelea, su mejor amigo, Tetsuo, sufre un extraño accidente y termina ingresado en unas instalaciones militares. Allí los científicos descubrirán que es el poseedor de la energía absoluta. Pero Tetsuo, que no se resigna a convertirse en un conejillo de indias, muy pronto se convertirá en la amenaza más grande que el mundo ha conocido.",
+                            "El adolescente Marty McFly es amigo de Doc, un científico al que todos toman por loco. Cuando Doc crea una máquina para viajar en el tiempo, un error fortuito hace que Marty llegue a 1955, año en el que sus futuros padres aún no se habían conocido. Después de impedir su primer encuentro, deberá conseguir que se conozcan y se casen; de lo contrario, su existencia no sería posible.",
+                            "Aunque a Marty McFly todavía le falta tiempo para asimilar el hecho de estar viviendo dentro de la familia perfecta gracias a su anterior viaje en el tiempo, no le queda ni espacio para respirar cuando su amigo Doc aparece de improviso con la máquina del tiempo (mucho más modernizada), e insta a que le acompañen él y su novia a viajar al futuro para solucionar un problema con la ley que tendrá uno de sus futuros hijos. En la tremenda vorágine futurista, con todo lo que ello conlleva, del Hill Valley de 2015, la presencia de tales viajeros temporales causará un efecto mayor que el que iban a arreglar. Un almanaque deportivo y la posesión del secreto de la existencia de la máquina del tiempo por parte del siempre villano Biff Tannen, serán los ingredientes que conjugarán una causa-efecto en el pasado, en el presente y en el propio futuro, que hará que Marty y Doc se tengan que emplear a fondo para poner fin a la catástrofe a la que les lleva el destino.",
+                            "Marty McFly sigue en 1955 y su amigo Doc ha retrocedido al año 1885, la época del salvaje oeste. Éste le envía una carta donde comenta que la máquina del tiempo está averiada, y que es imposible repararla. Sin embargo no le preocupa estar atrapado en el pasado, pues allí es muy feliz trabajando de herrero aunque convive con malhechores. Pero Marty descubre una vieja tumba en la que lee que Doc murió en 1885 y, sin pensárselo dos veces, irá a rescatar a su amigo.",
+                            "La Tierra, año 2084. Doug Quaid, un hombre que lleva una vida aparentemente tranquila, vive atormentado por una pesadilla que todas las noches lo transporta a Marte. Decide entonces recurrir al laboratorio de Recall, una empresa de vacaciones virtuales que le ofrece la oportunidad de materializar su sueño gracias a un fuerte alucinógeno, pero la droga hace aflorar a su memoria una estancia verdadera en Marte cuando era el más temido agente del cruel Coohagen.",
+                            "Cuenta la historia de un policía renegado (llamado Kung Fury) que persigue a su archienemigo (Hitler) a través del tiempo. Comedia de acción que rinde homenaje al cine ochentero. Incluye dinosaurios, nazis, vikingos y robots.",
+                            "Los Ángeles, año 2029. Las máquinas dominan el mundo. Los rebeldes que luchan contra ellas tienen como líder a John Connor, un hombre que nació en los años ochenta. Para acabar con la rebelión, las máquinas deciden enviar al pasado a un robot -Terminator- cuya misión será eliminar a Sarah Connor, la madre de John, e impedir así su nacimiento.",
+                            "Sarah Connor, la madre soltera del rebelde John Connor, está ingresada en un psiquiátrico. Algunos años antes, un viajero del tiempo le había revelado que su hijo sería el salvador de la humanidad en un futuro dominado por las máquinas. Se convirtió entonces en una especie de guerrera y educó a su hijo John en tácticas de supervivencia. Esta es la razón por la que está recluida en un manicomio. Cuando un nuevo androide mejorado, un T-1000, llega del futuro para asesinar a John, un viejo modelo T-800 será enviado para protegerle.",
+                            "Thomas Anderson es un brillante programador de una respetable compañía de software. Pero fuera del trabajo es Neo, un hacker que un día recibe una misteriosa visita...",
+                            "Cuando Sam Flynn (Garrett Hedlund), un experto programador de 27 años, investiga la desaparición de su padre, Kevin Flynn (Jeff Bridges), se encuentra de repente inmerso en un peligroso y salvaje mundo surrealista, un mundo paralelo donde su padre ha vivido durante 25 años. Con la ayuda de una joven (Olivia Wilde), padre e hijo emprenden un viaje a vida o muerte, a través de un sofisticado universo cibernético. Secuela del clásico de culto de 1982."
+                            );
+
+            $puntuaciones = array(10,10,10,10,7,10,8,10,10,5);
+
+            echo "<div class='contenedor'>
+            <div class='primera_caja'>
+                <h1>CIENCIA FICCIÓN</h1>
+                <a href='index.php'>INICIO</a>
+            </div>";
+            for ($i=0; $i < count($posters); $i++) { 
+                
+                $sinopsisReducida = substr($sinopsis[$i], 0, 280);
+
+                echo "<div class='segunda_caja'>
+                <div class='bordeIzquierdo'></div>
+                <div class='primera_columna'>
+                    <h1 class='titulo'>$titulos[$i]</h1>
+                    <img src='$posters[$i]'>
+                    <p class='duracion'>$duraciones[$i].</p>
+                </div>
+                <div class='segunda_columna'>
+                    <p>
+                        $sinopsisReducida ...
+                    </p>
+                </div>
+                <div class='tercera_columna'>
+                <div class='puntuacion'>
+                    <p>Puntuación: $puntuaciones[$i]</p>
+                    <p>Tu puntuación</p>
+                    <select name='notas' id='notas'>
+                        <option value='No vista'>No vista</option>
+                        <option value='10'>10 Excelente</option>
+                        <option value='9'>9 Muy buena</option>
+                        <option value='8'>8 Notable</option>
+                        <option value='7'>7 Buena</option>
+                        <option value='6'>6 Interesante</option>
+                        <option value='5'>5 Pasable</option>
+                        <option value='4'>4 Regular</option>
+                        <option value='3'>3 Floja</option>
+                        <option value='2'>2 Mala</option>
+                        <option value='1'>1 Muy mala</option>
+                    </select>
+                </div>
+            <div class='verFicha'>
+                <a href=''>Ver Ficha</a>
+            </div>
+        </div>
+        <div class='bordeDerecho'></div>
+        </div>";
+        }
+        echo "<div class='tercera_caja'></div>";
+
+            }
+
         ?>
         
 </body>
