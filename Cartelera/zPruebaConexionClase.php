f6e0dd1c9c467b2cb4e6013d5454775c9c9094cc
carlos_blanco <carlosblancofranco@gmail.com>
2022-11-29 20:49:23 +0100

    29 noviembre clase

--- Cartelera/zPruebaConexionClase.php
+++ Cartelera/zPruebaConexionClase.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,7 +11,67 @@
         ini_set('display_errors', 'On');
         ini_set('html_errors', 0);
 
-        // Contraseña->Casa: 12345678 | Clase: 12345
+        class Pelicula{
+
+            public $id;
+            public $titulo;
+            public $anyo;
+            public $duracion;
+            public $sinopsis;
+            public $imagen;
+            public $votos;
+            public $id_categoria;
+
+            public function __construct($id, $titulo, $anyo, $duracion, $sinopsis, $imagen, $votos, $id_categoria){
+
+                $this->id=$id;
+                $this->titulo=$titulo;
+                $this->anyo=$anyo;
+                $this->duracion=$duracion;
+                $this->sinopsis=$sinopsis;
+                $this->imagen=$imagen;
+                $this->votos=$votos;
+                $this->id_categoria=$id_categoria;
+                
+            }
+
+            public function getId(){
+                return $this->id;
+            }
+    
+            public function getTitulo(){
+                return $this->titulo;
+            }
+
+            public function getAnyo(){
+                return $this->anyo;
+            }
+
+            public function getDuracion(){
+                return $this->duracion;
+            }
+
+            public function getSinopsis(){
+                return $this->sinopsis;
+            }
+
+            public function getImagen(){
+                return $this->imagen;
+            }
+            
+            public function getVotos(){
+                return $this->votos;
+            }
+
+            public function getIdCategoria(){
+                return $this->id_categoria;
+            }
+
+
+        }
+
+        function cargarDatos(){
+            // Contraseña->Casa: 12345678 | Clase: 12345
         $conexion = mysqli_connect('localhost', 'root', '12345');
         
         mysqli_select_db($conexion, 'prueba');
@@ -26,24 +85,51 @@
             $mensaje = 'Consulta realizada: ' . $consulta;
             die($mensaje);
         } else{
-            echo "Conexion OK.<br>";
+            //echo "Conexion OK.<br>";
 
+            $peliculas = [];
+            $contador = 1;
             while ($registro = mysqli_fetch_assoc($resultado)) {
-                echo $registro['ID']."<br>";
-                echo $registro['titulo']."<br>";
-                echo $registro['año']."<br>";
-                echo $registro['duracion']."<br>";
-                echo $registro['sinopsis']."<br>";
-                echo $registro['imagen']."<br>";
-                echo $registro['votos']."<br>";
-                echo $registro['id_categoria']."<br>";
+
+                $peliculas[$contador] = new Pelicula($registro['ID'], $registro['titulo'], $registro['año'], $registro['duracion'], $registro['sinopsis'], $registro['imagen'], $registro['votos'], $registro['id_categoria']);
+                $contador = $contador + 1;
+                
             }
+
+            return $peliculas;
+            }
+
         }
-/*
-        if (mysqli_connect_error()) {
-            echo "Error al conectar a MySQL: ".mysqli_connect_error();
-        }
-*/
+
+
+
+        function mostrarDatos(){
+            
+            $datos = cargarDatos();
+            $peliculas = [];
+            for ($i=1; $i < count($datos); $i++) { 
+
+                $peliculas[$i] = $datos[$i];
+                /*
+                echo $datos[$i]->getId()."<br>";
+                echo $datos[$i]->getTitulo()."<br>";
+                echo $datos[$i]->getAnyo()."<br>";
+                echo $datos[$i]->getDuracion()."<br>";
+                echo $datos[$i]->getSinopsis()."<br>";
+                echo $datos[$i]->getDuracion()."<br>";
+                echo $datos[$i]->getVotos()."<br>";
+                echo $datos[$i]->getIdCategoria()."<br><br><br>";*/
+
+            }
+            return $peliculas;
+
+        } 
+
+        $peliculas = mostrarDatos();
+
+        echo $peliculas[1]->getTitulo();
+
+
     ?>
 
 </body>
