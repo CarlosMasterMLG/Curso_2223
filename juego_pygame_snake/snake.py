53b14406347f79120ce8fc9881f9502fbe8468b3
carlos_blanco <carlosblancofranco@gmail.com>
2022-12-21 16:27:12 +0100

    21 diciembre clase

--- juego_pygame_snake/snake.py
+++ juego_pygame_snake/snake.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from pygame.locals import *
 
 if __name__ == "__main__":
@@ -7,8 +8,8 @@ if __name__ == "__main__":
     surface.fill((255,255,255))
     pygame.display.flip() 
 
-    location_x = 50
-    location_y = 50
+    location_x = 450
+    location_y = 200
     color = (255,0,0)
     exit = False
     while not exit:
@@ -21,15 +22,28 @@ if __name__ == "__main__":
             if event.type == pygame.KEYDOWN:
                 
                 if event.key == pygame.K_RIGHT:
-                    print("Key RIGHT has been pressed")
-                    location_x+=1
+                    print("Key_RIGHT has been pressed")
+                    location_x+=10
+
+                    #while location_x < 1000:
+                        #location_x+=10
+                        #time.sleep(1)
+
+
                 if event.key == pygame.K_DOWN:
                     print("Key K_DOWN has been pressed")
-                    location_y+=1
-                
+                    location_y+=10
+                if event.key == pygame.K_LEFT:
+                    print("Key_LEFT has been pressed")
+                    location_x-=10
+                if event.key == pygame.K_UP:
+                    print("Key_UP has been pressed")
+                    location_y-=10
         #Pintar
         surface.fill((255,255,255))
         pygame.draw.rect(surface, color, pygame.Rect(location_x, location_y, 60, 60))
+        pygame.draw.rect(surface, color, pygame.Rect(location_x-50, location_y+5, 50, 50))
+        pygame.draw.rect(surface, color, pygame.Rect(location_x-90, location_y+10, 40, 40))
         pygame.display.flip()
                
 
