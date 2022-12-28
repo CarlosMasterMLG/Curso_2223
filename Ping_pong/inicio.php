68d4752907b803090ea4d731f33962f12f8970c2
carlos_blanco <carlosblancofranco@gmail.com>
2022-12-28 18:19:02 +0100

    28 diciembre casa

---
+++ Ping_pong/inicio.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/inicio.css">
+    <link rel="shortcut icon" href="imgs/raqueta.jpg" type="image/jpg">
+    <title>INICIO</title>
+</head>
+<body>
+    <div class="contenedor">
+        <div class="primera_caja"></div>
+        <div class="segunda_caja">
+            <div class="primera_columna">
+                <div class="cube">
+                    <div class="top"></div>
+                    <div>
+                        <span style="--i:0;"></span>
+                        <span style="--i:1;"></span>
+                        <span style="--i:2;"></span>
+                        <span style="--i:3;"></span>
+                        </div>
+                    </div>
+            </div>
+            <div class="segunda_columna">
+                    <h1 class="neon-title">Carlos Sport Tournament</h1>
+                    <a href="torneo.php" class="neon-button">Enlace</a>
+            </div>
+            <div class="tercera_columna">
+                <div class="cube">
+                    <div class="top"></div>
+                    <div>
+                        <span style="--i:0;"></span>
+                        <span style="--i:1;"></span>
+                        <span style="--i:2;"></span>
+                        <span style="--i:3;"></span>
+                    </div>
+                </div>
+            </div>      
+        </div>
+    </div>
+</body>
+</html>
