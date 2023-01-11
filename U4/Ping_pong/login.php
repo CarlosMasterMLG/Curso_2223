87282aca91022aab84ce14f94c2cb32f67f5034c
carlos_blanco <carlosblancofranco@gmail.com>
2023-01-11 18:22:07 +0100

    11 enero clase

---
+++ U4/Ping_pong/login.php
@@ -0,0 +1,49 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="shortcut icon" href="imgs/raqueta.jpg" type="image/jpg">
+    <title>Login</title>
+</head>
+<body>
+    <div class="contenedor">
+        <div class="primera_caja"></div>
+        <div class="segunda_caja">
+            <div class="primera_columna">
+                <div class="cube">
+                    <div class="top"></div>
+                        <div>
+                            <span style="--i:0;"></span>
+                            <span style="--i:1;"></span>
+                            <span style="--i:2;"></span>
+                            <span style="--i:3;"></span>
+                        </div>
+                    </div>
+            </div>
+            <div class="segunda_columna">
+                <div class="container">
+                    <h2>Usuario</h2>
+                    <input type="name" placeholder="name" id="myName">
+                    <h2>Contraseña</h2>
+                    <input type="password" placeholder="password" id="myPassword">
+
+                    <a href="torneo.php" class="neon-button">INICIAR</a>
+                </div>
+            </div>
+            <div class="tercera_columna">
+                <div class="cube">
+                    <div class="top"></div>
+                    <div>
+                        <span style="--i:0;"></span>
+                        <span style="--i:1;"></span>
+                        <span style="--i:2;"></span>
+                        <span style="--i:3;"></span>
+                    </div>
+                </div>
+            </div>      
+        </div>
+    </div>
+</body>
+</html>
