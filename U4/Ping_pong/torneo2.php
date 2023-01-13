638101eb11da24b875e7660809c5ad3a8e6e3ff3
carlos_blanco <carlosblancofranco@gmail.com>
2023-01-13 16:06:07 +0100

    13 enero clase

---
+++ U4/Ping_pong/torneo2.php
@@ -0,0 +1,40 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/torneo2.css">
+    <title>Torneo</title>
+</head>
+<body>
+    <div class='contenedor'>
+
+        <div class='borde'></div>
+        <div class='primera_ronda'>
+            <div class='partido1'>Carlos</div>
+            <div class='partido1'>Carlos</div>
+            <div class='partido2'>Carlos</div>
+            <div class='partido2'>Carlos</div>
+            <div class='partido3'>Carlos</div>
+            <div class='partido3'>Carlos</div>
+            <div class='partido4'>Carlos</div>
+            <div class='partido4'>Carlos</div>
+        </div>
+        <div class='segunda_ronda'>
+            <div class='ganador1'>Carlos</div>
+            <div class='ganador2'>Carlos</div>
+            <div class='ganador3'>Carlos</div>
+            <div class='ganador3'>Carlos</div>
+        </div>
+        <div class='tercera_ronda'>
+            <div class='ganador5'>Carlos</div>
+            <div class='ganador6'>Carlos</div>
+        </div>
+        <div class='cuarta_ronda'>
+            <div class='ganador9'>Carlos</div>
+        </div>
+        <div class='borde'></div>
+        
+    </div>
+</body>
+</html>
