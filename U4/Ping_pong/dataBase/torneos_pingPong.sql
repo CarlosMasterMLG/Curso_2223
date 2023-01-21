897aca25cbe3139c72cd850551227ffd35653cde
CarlosMasterMLG <carlosblancofranco@gmail.com>
2023-01-21 15:03:31 +0100

    21 enero casa

---
+++ U4/Ping_pong/dataBase/torneos_pingPong.sql
@@ -0,0 +1,80 @@
+drop database if exists torneos_pingPong;
+create database torneos_pingPong;
+
+use torneos_pingPong;
+
+CREATE TABLE T_Usuario (
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    contrasenya VARCHAR(20) NOT NULL
+);
+
+insert into T_Usuario (nombre, contrasenya) values ('Gandalf el gris', '1234');
+insert into T_Usuario (nombre, contrasenya) values ('Bilbo Bolsón', '1234');
+insert into T_Usuario (nombre, contrasenya) values ('Aragorn heredero de Isildur', '1234');
+insert into T_Usuario (nombre, contrasenya) values ('Tom Bombadil', '1234');
+insert into T_Usuario (nombre, contrasenya) values ('Cebadilla Mantecona', '1234');
+insert into T_Usuario (nombre, contrasenya) values ('Túrin hijo de Húrin', '1234');
+insert into T_Usuario (nombre, contrasenya) values ('Melkor', '1234');
+insert into T_Usuario (nombre, contrasenya) values ('Ilúvatar', '1234');
+
+select * from T_Usuario;
+
+CREATE TABLE T_Torneo (
+    id_torneo INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    num_jugadores INT DEFAULT 8,
+    fecha DATE NOT NULL
+);
+
+insert T_Torneo (nombre, fecha) values ('Torneo navidad', '2023-01-09');
+insert T_Torneo (nombre, fecha) values ('Torneo fin de curso', '2023-06-15');
+
+select * from T_Torneo;
+
+CREATE TABLE T_Jugador (
+    id_jugador INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    total_partidos INT NOT NULL,
+    partidos_ganados INT NOT NULL,
+    total_torneos INT NOT NULL,
+    torneos_ganados INT NOT NULL
+);
+
+insert into T_Jugador (nombre, total_partidos, partidos_ganados, total_torneos, torneos_ganados) values ('Gandalf el gris', 0, 0, 0, 0);
+insert into T_Jugador (nombre, total_partidos, partidos_ganados, total_torneos, torneos_ganados) values ('Bilbo Bolsón', 0, 0, 0, 0);
+insert into T_Jugador (nombre, total_partidos, partidos_ganados, total_torneos, torneos_ganados) values ('Aragorn heredero de Isildur', 0, 0, 0, 0);
+insert into T_Jugador (nombre, total_partidos, partidos_ganados, total_torneos, torneos_ganados) values ('Tom Bombadil', 0, 0, 0, 0);
+insert into T_Jugador (nombre, total_partidos, partidos_ganados, total_torneos, torneos_ganados) values ('Cebadilla Mantecona', 0, 0, 0, 0);
+insert into T_Jugador (nombre, total_partidos, partidos_ganados, total_torneos, torneos_ganados) values ('Túrin hijo de Húrin', 0, 0, 0, 0);
+insert into T_Jugador (nombre, total_partidos, partidos_ganados, total_torneos, torneos_ganados) values ('Melkor', 0, 0, 0, 0);
+insert into T_Jugador (nombre, total_partidos, partidos_ganados, total_torneos, torneos_ganados) values ('Ilúvatar', 0, 0, 0, 0);
+
+select * from T_Jugador;
+
+CREATE TABLE T_Partido (
+    id_partido INT PRIMARY KEY AUTO_INCREMENT,
+    id_torneo INT,
+    tipo_partido ENUM('cuartos', 'semifinal', 'final'),
+    id_jugador_a INT,
+    id_jugador_b INT,
+    ganador INT #,
+    #FOREIGN KEY (id_torneo) REFERENCES T_Torneo (id_torneo),
+    #FOREIGN KEY (id_jugador_a) REFERENCES T_Jugador (id_jugador),
+    #FOREIGN KEY (id_jugador_b)  REFERENCES T_Jugador (id_jugador),
+    #FOREIGN KEY (ganador) REFERENCES T_Jugador (id_jugador)
+);
+
+insert into T_Partido (id_torneo, tipo_partido, id_jugador_a, id_jugador_b, ganador) values ('1', 'cuartos', 7, 3, 7);
+insert into T_Partido (id_torneo, tipo_partido, id_jugador_a, id_jugador_b, ganador) values ('2', 'cuartos', 1, 4, 1);
+insert into T_Partido (id_torneo, tipo_partido, id_jugador_a, id_jugador_b, ganador) values ('3', 'cuartos', 6, 8, 8);
+insert into T_Partido (id_torneo, tipo_partido, id_jugador_a, id_jugador_b, ganador) values ('4', 'cuartos', 5, 2, 2);
+insert into T_Partido (id_torneo, tipo_partido, id_jugador_a, id_jugador_b, ganador) values ('5', 'semifinal', 7, 1, 1);
+insert into T_Partido (id_torneo, tipo_partido, id_jugador_a, id_jugador_b, ganador) values ('6', 'semifinal', 8, 2, 8);
+insert into T_Partido (id_torneo, tipo_partido, id_jugador_a, id_jugador_b, ganador) values ('7', 'final', 1, 8, 8);
+
+select * from T_Partido;
+
+
+
+
--- U4/Ping_pong/ping_pong.sql
+++
@@ -1,6 +0,0 @@
-create database ping_pong;
-use ping_pong;
-
-create table jugadores(
-	
-);
