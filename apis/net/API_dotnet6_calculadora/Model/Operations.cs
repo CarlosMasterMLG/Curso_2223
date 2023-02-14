a3e48324c562c67c4d9b5906497bee6c3304babe
carlos_blanco <carlosblancofranco@gmail.com>
2023-02-14 20:25:55 +0100

    14 febrero casa

--- apis/net/API_dotnet6_calculadora/Model/Operations.cs
+++ apis/net/API_dotnet6_calculadora/Model/Operations.cs
@@ -6,5 +6,22 @@ namespace Model
         {
             return (base_triangulo * altura) / 2;
         }
+
+        function obtenerFactorial(int numero){
+
+                int resultado = 1;
+                
+                if(numero == 0){
+                    return 1;
+                } else if(numero > 0){
+                    
+                    while(numero > 0){
+                        resultado = resultado * numero;
+                        numero--;
+                    }
+                    return resultado;
+                } 
+
+            }
     }
 }
